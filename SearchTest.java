e85eb88b6f71c4a1bd1238ffc180802796fdfef2
klyul <125492959+klyul@users.noreply.github.com>
2023-02-15 23:31:38 +0300

    hw2

--- SearchTest.java
+++ SearchTest.java
@@ -8,7 +8,7 @@ public class SearchTests {
     @Test
     void successfulSearchTest() {
         open("https://www.google.com/");
-        $("[name=q]").setValue("selenide").pressEnter();
+        $("[name=q]").setValue("seleniede").pressEnter();
         $("[id=search]").shouldHave(text("https://selenide.ru"));
     }
 
